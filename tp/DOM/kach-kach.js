4cf91fe91a657b752834d23df61e2d6ff141ee8c
Pierre Chris Wesley <pierrechriswesley@gmail.com>
2022-11-23 22:01:37 -0500

    dom manipulation

---
+++ tp/DOM/kach-kach.js
@@ -0,0 +1,11 @@
+//Firstly
+// let idWrapper = document.getElementById('wrapper')
+// idWrapper.querySelector('div:nth-child(3)>span').textContent = "kachèt révélé";
+
+//Secondly
+// let wrapperDiv = document.getElementsByClassName('div-wrapper')
+// console.log(wrapperDiv[0].getElementsByTagName('div').item(3).textContent = "kachèt révélé");
+
+//Thirdly
+//document.querySelectorAll('span').item(1).textContent = "kachèt révélé"
+        
